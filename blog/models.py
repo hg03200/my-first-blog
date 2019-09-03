1d9c2e047a3f43daa480682d389a46db65f10324
nahui <hg03200@naver.com>
2019-09-03 13:49:29 +0900

    My Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200) # 글자 수가 제한된 텍스트
+    text = models.TextField() # 글자 수 제한되지 않은 텍스트
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
