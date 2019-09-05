cc5f9417ec3aeb22e74792ad4b81cb9d30dff9fa
nahui <hg03200@naver.com>
2019-09-05 11:17:39 +0900

    Added view and template for detailed blog post as well as CSS for the site

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # post_detail 뷰로 보내 게시글이 보일 수 있게 함
+    # <int:pk> : 장고는 정수 값을 기대하고 이를 pk라는 변수로 뷰로 전송
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -7,3 +7,7 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts':posts})
     # 글 목록이 있는 posts 변수를 템플릿에 넘기기
     # 이 후 템플릿이 posts 변수를 받아 HTML에 나타냄 (파이썬을 html로 변경해줌)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
